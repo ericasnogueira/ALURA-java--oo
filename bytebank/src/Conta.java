e0f0810d5e543bc1a788a9bc629b5021caaf746e
ericasnogueira <ericasnogueira10@gmail.com>
2022-07-21 13:52:05 -0300

    atualização de Conta e TestaMetodo

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -12,4 +12,30 @@ public class Conta {
     void deposita(double deposito){
        this.saldo += deposito; // a palavra this não está no deposito pq ela não é um atributo de um objeto
     }
+
+    //quais informações você precisa passar ?
+    // se conseguiu sacar ou não// pq pode ter dinheiro ou não na conta
+
+    public boolean saca(double valor){ // na parte do boolean está prometendo que ira retorna um boolean
+        // tem dinheiro na conta
+        if (this.saldo >= valor){ // this.saldo o saldo da conta
+            this.saldo -=  valor; // this.saldo = this.saldo - valor
+            return true;
+        } else {
+            System.out.println("Não tem dinheiro suficiente na conta");
+            return  false;
+        }
+
+    }
+    public boolean transfere(double valor, Conta destino){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            destino.deposita(valor);
+            System.out.println("Transferencia feita com sucesso ");
+            return true;
+        }else {
+            System.out.println("Não tem dinheiro suficiente para a transferencia");
+            return  false;
+        }
+    }
 }
--- bytebank/src/TestaMetodo.java
+++ bytebank/src/TestaMetodo.java
@@ -5,8 +5,28 @@ public class TestaMetodo {
         contaUsuario.saldo = 100;
         contaUsuario.deposita(50);// depositando
         contaUsuario.deposita(500);
-        System.out.println(contaUsuario.saldo);
+        System.out.println("valor em saldo disponivel : "+contaUsuario.saldo);
         Conta contaErica = new Conta();
-        System.out.println(contaErica.saldo);
+        System.out.println("Valor do saldo disponivel conta Erica : "+contaErica.saldo);
+
+        // tirando 20 da conta
+        boolean conseguiuRetira = contaUsuario.saca(20); // guardando o valor dentro de uma variavel
+        System.out.println("valor em saldo disponivel depois da retirada referente a conta Usuario : " +contaUsuario.saldo);
+
+        System.out.println(conseguiuRetira);// irar aparecer true ou false como foi feito no metodo
+
+        System.out.println();// pulando linha
+
+        //transferir valor de uma conta para outra
+        //transferir um valor(300) da conta usuario para conta erica
+        System.out.println("Tranferindo o valor da conta Usuario para conta Erica");
+        //transferencia foi ?
+        contaUsuario.transfere(300,contaErica);
+
+        System.out.println("Valor atual da conda Usuario : "+contaUsuario.saldo);
+        System.out.println("Valor atual da conta Erica : " + contaErica.saldo);
+
+
+
     }
 }
