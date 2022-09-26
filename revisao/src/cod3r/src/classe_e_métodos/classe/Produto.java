9c242c251cff09c03a9cb9b35311c21666ffb4ec
erica nogueira <ericasnogueira10@gmail.com>
2022-09-26 09:47:38 -0300

    desafio de membros de classe vs instancia

--- revisao/src/cod3r/src/classe_e_métodos/classe/Produto.java
+++ revisao/src/cod3r/src/classe_e_métodos/classe/Produto.java
@@ -4,17 +4,29 @@ public class Produto {
     //atributos
     String nome;
     double preco;
-    double desconto;
+    static double desconto = 0.25;
 
+    public Produto(){
 
+    }
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
 
     //metodo para o desconto
     // botar o metodo aqui foi minha ideia, já que no curso ainda não tinha falado sobre isso
-    public double valorFinal(){
+    public double valorFinal(){ // preço com o desconto
         double valorInicial = preco * desconto;
         double valorFinal;
         return valorFinal = preco - valorInicial;
     }
+    public double valorFinal(double gerente){
+        return preco * (1 - desconto + gerente);
+    }
+
+
 
 
 }
--- revisao/src/cod3r/src/classe_e_métodos/classe/ProdutoTeste.java
+++ revisao/src/cod3r/src/classe_e_métodos/classe/ProdutoTeste.java
@@ -8,28 +8,26 @@ public class ProdutoTeste {
         Locale.setDefault(Locale.US);
         Scanner entrada = new Scanner(System.in);
 
-        Produto produto = new Produto();
+        Produto produto = new Produto("Notebook", 4356.89); //
         Produto produto2 = new Produto();
-
-        produto.nome = "Acer nitro 5";
-        produto.preco = 4356.89;
-        produto.desconto = 0.25;
 
         produto2.nome = "Caneta preta";
         produto2.preco = 12.56;
-        produto2.desconto = 0.29;
 
+        Produto.desconto = 0.50; //modificando o desconto de todos os produtos
 
-        System.out.println(produto2.nome);
         System.out.println(produto.nome);
+        System.out.println(produto2.nome);
+
 
         //valores com o desconto
         System.out.printf("valor final  do acer nitro 5 foi de : %.2f%n", produto.valorFinal());
-        System.out.printf("valor final da caneta  foi de : %.2f%n", produto2.valorFinal());
+        System.out.printf("valor final da caneta  foi de : %.2f%n", produto2.valorFinal(0.1));
 
         //media da compra
         double media = (produto.valorFinal() + produto2.valorFinal()) / 2;
-        System.out.printf("Media da compra foi de R$ %.2f", media);
+        System.out.printf("Media da compra foi de R$ %.2f%n", media);
+
 
     }
 }
